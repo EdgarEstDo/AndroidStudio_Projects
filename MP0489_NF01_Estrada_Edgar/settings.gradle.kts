b4883ddc61c6f2efed360a0778b14ae98554873f
Edgar Estrada Dorrego <edgared@Mac-mini-de-Edgar.local>
2026-03-10 12:56:01 +0100

    New Project - NF01 Deliverable

---
+++ MP0489_NF01_Estrada_Edgar/settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MP0489_NF01_Estrada_Edgar"
+include(":app")
