3aba9bef090ca5adcca034c4776ee4af4a1a90b1
Edgar Estrada Dorrego <edgared@Mac-mini-de-Edgar.local>
2026-04-06 15:25:27 +0200

    NF2 updates

--- 0420260_NF2EstEdgar/app/src/main/java/com/example/a042026_0_nf2_estedgar/MainActivity.java
+++ 0420260_NF2EstEdgar/app/src/main/java/com/example/a042026_0_nf2_estedgar/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a042026_0_nf2_estedgar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Variables globales
+    private TextView suma_random;
+    private Button result1;
+    private Button result2;
+    private Button result3;
+    private int resultado;
+    private int valor1;
+    private int valor2;
+    private int botonAleatorio;
+
+    //Random para generar números aleatorios
+    Random random = new Random();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Inicialización de variables
+        suma_random =  findViewById(R.id.textView_main);
+        result1 =  findViewById(R.id.button_main);
+        result2 =  findViewById(R.id.button2_main);
+        result3 =  findViewById(R.id.button3_main);
+        resultado = 0;
+        valor1 = 0;
+        valor2 = 0;
+
+        //Generación de números aleatorios para escoger los botones
+        botonAleatorio = random.nextInt(3 - 1 + 1) + 1;
+
+        if(botonAleatorio == 1){
+            result1.setText(String.valueOf(resultado));
+            result2.setText(String.valueOf(resultado + 1));
+            result3.setText(String.valueOf(resultado - 1));
+        }else if(botonAleatorio == 2){
+            result2.setText(String.valueOf(resultado));
+            result1.setText(String.valueOf(resultado + 1));
+            result3.setText(String.valueOf(resultado - 1));
+        }else if(botonAleatorio == 3){
+            result3.setText(String.valueOf(resultado));
+            result1.setText(String.valueOf(resultado + 1));
+            result2.setText(String.valueOf(resultado - 1));
+        }
+
+        }
+
+
+
+
+
+
+
+
     }
-}
